fa3feae9375c6db262a649d1654ea6efbcb0f4be
atharva-2001 <aryaa_1@rknec.edu>
2023-03-29 13:35:24 +0530

    modify benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -9,7 +9,7 @@ class TimeSuite:
     """
     def setup(self):
         self.d = {}
-        for x in range(5):
+        for x in range(800):
             self.d[x] = None
 
     def time_keys(self):

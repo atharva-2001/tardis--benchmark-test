38abcbaeba314e0ebb8341b7f4a8890987bcc47f
atharva-2001 <aryaa_1@rknec.edu>
2023-03-29 13:28:42 +0530

    delete

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -2,35 +2,32 @@
 # See "Writing benchmarks" in the asv docs for more information.
 from tbt import run_this
 
-# class TimeSuite:
-#     """
-#     An example benchmark that times the performance of various kinds
-#     of iterating over dictionaries in Python.
-#     """
-#     def setup(self):
-#         self.d = {}
-#         for x in range(5):
-#             self.d[x] = None
-
-#     def time_keys(self):
-#         for key in self.d.keys():
-#             pass
-
-#     def time_values(self):
-#         for value in self.d.values():
-#             pass
-
-#     def time_range(self):
-#         d = self.d
-#         for key in range(800):
-#             x = d[key]
-
-class testbench:
+class TimeSuite:
+    """
+    An example benchmark that times the performance of various kinds
+    of iterating over dictionaries in Python.
+    """
     def setup(self):
-        pass
+        self.d = {}
+        for x in range(5):
+            self.d[x] = None
+
+    def time_keys(self):
+        for key in self.d.keys():
+            pass
+
+    def time_values(self):
+        for value in self.d.values():
+            pass
+
+    def time_range(self):
+        d = self.d
+        for key in range(800):
+            x = d[key]
 
     def time_runthis(self):
         _ =  run_this()
+
 
 # class MemSuite:
 #     def mem_list(self):

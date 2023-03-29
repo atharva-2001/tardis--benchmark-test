ea7024843c5e3c0907db0b9e1a67a88b4a6c4dc3
atharva-2001 <aryaa_1@rknec.edu>
2023-03-29 13:21:41 +0530

    modify benchmarks

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,33 +1,37 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
+from tbt import run_this
 
+# class TimeSuite:
+#     """
+#     An example benchmark that times the performance of various kinds
+#     of iterating over dictionaries in Python.
+#     """
+#     def setup(self):
+#         self.d = {}
+#         for x in range(5):
+#             self.d[x] = None
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+#     def time_keys(self):
+#         for key in self.d.keys():
+#             pass
+
+#     def time_values(self):
+#         for value in self.d.values():
+#             pass
+
+#     def time_range(self):
+#         d = self.d
+#         for key in range(800):
+#             x = d[key]
+
+class testbench:
     def setup(self):
-        self.d = {}
-        for x in range(5000):
-            self.d[x] = None
+        pass
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
+    def time_runthis(self):
+        _ =  run_this()
 
-    def time_values(self):
-        for value in self.d.values():
-            for random_no in range(30000):
-                _ = random_no**random_no
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(800):
-            x = d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+# class MemSuite:
+#     def mem_list(self):
+#         return [0] * 256
--- tbt/base.py
+++ tbt/base.py
@@ -1,4 +1,11 @@
-from .cli import main  # pragma: no cover
+# from .cli import main  # pragma: no cover
 
-if __name__ == "__main__":  # pragma: no cover
-    main()
+def run_this(no=10):
+    lst = []
+    for item in no:
+        lst.append(no*2)
+    del lst
+    
+
+# if __name__ == "__main__":  # pragma: no cover
+#     main()
